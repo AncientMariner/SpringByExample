efb053218389a2d3506e0fa628c62406113d904c
AncientMariner <xander.lysak@gmail.com>
2013-11-11 20:45:55 +0200

    Securing Methods with Annotations

--- src/main/java/org/xander/spring/springIdol/springSecurity/service/MessageBoardServiceImpl.java
+++ src/main/java/org/xander/spring/springIdol/springSecurity/service/MessageBoardServiceImpl.java
@@ -1,5 +1,6 @@
 package org.xander.spring.springIdol.springSecurity.service;
 
+import org.springframework.security.access.annotation.Secured;
 import org.xander.spring.springIdol.springSecurity.domain.Message;
 
 import java.util.ArrayList;
@@ -8,17 +9,26 @@ import java.util.List;
 import java.util.Map;
 
 public class MessageBoardServiceImpl implements MessageBoardService {
+
     private Map<Long, Message> messages = new LinkedHashMap<>();
+
+    @Secured({"ROLE_USER", "ROLE_GUEST"})
     public List<Message> listMessages() {
         return new ArrayList<Message>(messages.values());
     }
+
+    @Secured("ROLE_USER")
     public synchronized void postMessage(Message message) {
         message.setId(System.currentTimeMillis());
         messages.put(message.getId(), message);
     }
+
+    @Secured({"ROLE_ADMIN", "IP_LOCAL_HOST"})
     public synchronized void deleteMessage(Message message) {
         messages.remove(message.getId());
     }
+
+    @Secured({"ROLE_USER", "ROLE_GUEST"})
     public Message findMessageById(Long messageId) {
         return messages.get(messageId);
     }
